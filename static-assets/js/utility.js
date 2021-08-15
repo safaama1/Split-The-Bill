845b06e0659144c8df99b717583ffe791b205b03
Saher-Amasha <saheramasha0@gmail.com>
2021-08-15 17:26:15 +0300

    profile.html now has access to user info

---
+++ static-assets/js/utility.js
@@ -0,0 +1,16 @@
+function getCookieObject(){
+	
+	return document.cookie
+		.split(';')
+		.reduce((res, c) => {
+			const [key, val] = c.trim().split('=').map(decodeURIComponent);
+			try {
+				return Object.assign(res, { [key]: JSON.parse(val) });
+			} catch (e) {
+				return Object.assign(res, { [key]: val });
+			}
+		}, {});
+}
+
+
+
